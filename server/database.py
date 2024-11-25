55715b5daf2ca3a69433d900d39b1b3779e909d4
JulakantiManith <julakantimanith1107@gmail.com>
2024-11-25 17:11:20 +0530

    db change

--- server/database.py
+++ server/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://manith:7crFlAONKonjFvCAhnWTySsdnfIM2yYY@dpg-cs9kgdo8fa8c73cbn0t0-a.oregon-postgres.render.com/fluttermusicapp'
+DATABASE_URL = 'postgresql://musicapp_0e0h_user:oKBj75Zr2D6U606iikegy2DDexJXnIM7@dpg-ct262ddsvqrc73e54230-a.oregon-postgres.render.com/musicapp_0e0h'
 #DATABASE_URL = 'postgresql://postgres:root@localhost/fluttermusicapp'
 
 engine = create_engine(DATABASE_URL)
